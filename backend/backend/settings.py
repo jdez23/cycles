88dbfce377add4b52e547f3c89164ab2d7fdd09b
Jesse Hernandez <jessemhernandez123@gmail.com>
2024-10-23 23:01:25 -0700

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'https://cyclesapp.s3.amazonaws.com/media/'
+MEDIA_URL = 'https://cyclesapp.s3.us-east-1.amazonaws.com/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 
