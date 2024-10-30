3ae0376d80876f0b1c94abe632136a7e58fdf166
Jesse Hernandez <jessemhernandez123@gmail.com>
2024-10-29 23:34:37 -0700

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,7 +175,6 @@ AWS_S3_SIGNATURE_NAME = os.environ['AWS_S3_SIGNATURE_NAME']
 AWS_S3_FILE_OVERWRITE = os.environ['AWS_S3_FILE_OVERWRITE']
 AWS_DEFAULT_ACL = os.environ['AWS_DEFAULT_ACL']
 AWS_QUERYSTRING_AUTH = os.environ['AWS_QUERYSTRING_AUTH']
-AWS_S3_VERIFY = os.environ['AWS_S3_VERIFY']
-AWS_LOCATION = 'media'
+# AWS_S3_VERIFY = os.environ['AWS_S3_VERIFY']
 
 django_heroku.settings(locals(), staticfiles=False)
