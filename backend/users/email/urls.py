c963045accb717e596a39b04d24b72b56d718d11
Jesse Hernandez <jessemhernandez123@gmail.com>
2024-12-16 23:56:30 -0800

    Make contact endpoint public

--- backend/users/email/urls.py
+++ backend/users/email/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from .views import send_contact_message
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 
-# Apply permission classes directly to the view
-send_contact_message_public = permission_classes(
-    [AllowAny])(send_contact_message)
-
 urlpatterns = [
-    path('contact/', send_contact_message_public, name='send_contact_message'),
+    path('contact/', authentication_classes([])
+         (permission_classes([AllowAny])(send_contact_message)), name='send_contact_message'),
 ]
